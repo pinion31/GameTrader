77a8f394f191420749dc5ca38a2fa5d203118ea6
Chris Cantu <pinion31@gmail.com>
2017-09-13 01:39:57 -0500

    updated to use axios-mock to mock http requests

--- __tests__/GameList_test.js
+++ __tests__/GameList_test.js
@@ -1,31 +1,29 @@
 import {renderComponent, expect} from './test_helper';
+import {Button, Modal, Thumbnail} from 'react-bootstrap';
 import React from 'react';
 import GameList from '../js/components/GameList';
 import GameItem from '../js/components/GameItem';
 import sinon from 'sinon';
-import TestUtils, {renderIntoDocument, findRenderedDOMComponentWithClass} from 'react-addons-test-utils';
+import TestUtils, {renderIntoDocument, findRenderedDOMComponentWithClass,
+  scryRenderedComponentsWithType, findRenderedDOMComponentWithTag} from 'react-addons-test-utils';
+//import * as TestUtils from 'react-addons-test-utils';
 import chaiHaveReactComponent from 'chai-have-react-component';
 import {addGame, getUserGames, removeGame, clearUserGames} from '../js/actions/gameActions';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from '../js/reducers/rootReducer';
 import thunk from 'redux-thunk';
-import chai from 'chai';
-import chaiJquery from 'chai-jquery';
-import _$ from 'jquery';
+import fetchMock from 'fetch-mock';
+import MockAdapter from 'axios-mock-adapter';
+import axios from 'axios';
 
-const ReactTestRenderer = require('react-test-renderer');
-global.window = global.document.defaultView;
-global.navigator = global.window.navigator;
-const $ = _$(window);
-
-chaiJquery(chai, chai.util, $);
-
-chai.use(chaiHaveReactComponent);
+const mock = new MockAdapter(axios);
 
 describe('GameList', () => {
-  let componentClass;
-  //let stubComponentDidMount;
+  let component;
+  let preComponent;
+  let stubComponentDidMount;
+  let getUserGamesStub;
   //let renderer;
 
   let mockGame = {
@@ -39,49 +37,43 @@ describe('GameList', () => {
 
   beforeEach(() => {
 
-    /*
-    stubComponentDidMount = sinon.stub(GameList, 'componentDidMount').returns({
-      type: 'GET_USER_GAMES', payload: [mockGame],
-    });*/
+    mock.onGet('/games/getUserGames').reply(200, [mockGame]);
 
     const middleware = applyMiddleware(thunk);
-
-      /*
-    stubComponentDidMount = stubComponentMethod(GameList, 'componentDidMount').returns({
-      type: 'GET_USER_GAMES', payload: [],
-    });*/
-
-/*
-    renderer = ReactTestRenderer.create(
-      <Provider store={createStore(reducers, middleware)}>
-        <GameList />
-      </Provider>
-    );*/
-
-    componentClass = renderIntoDocument(
+    component = renderIntoDocument(
       <Provider store={createStore(reducers, middleware)}>
         <GameList />
       </Provider>);
-
   });
 
   afterEach(() => {
-    //stubComponentDidMount.restore();
+    mock.restore();
   });
 
-  /*
-  it('shows a title of My Games', () => {
-    expect(componentClass.find('h1')).to.exist;
-  });*/
+  it('contains a Button component', () => {
+    const button = findRenderedDOMComponentWithTag(component, 'button');
+    expect(button).to.be.ok;
+    //expect(component.find('.accept-button').length).to.equal(1);
+  });
 
-  /*
-  it('shows a game lib containing Fallout 4', () => {
-    // expect(componentClass).to.have.class('game-item');
-  });*/
+   it('contains classnames,accept-button and add-game-button', () => {
+    const acceptButton = findRenderedDOMComponentWithClass(component, 'accept-button');
+    const addGameButton = findRenderedDOMComponentWithClass(component, 'add-game-button');
+    expect(acceptButton).to.be.ok;
+    expect(addGameButton).to.be.ok;
+  });
 
   it('contains class name called section-header', () => {
-    const sectionHeader = findRenderedDOMComponentWithClass(componentClass, 'section-header');
+    const sectionHeader = findRenderedDOMComponentWithClass(component, 'section-header');
     expect(sectionHeader).to.be.ok;
+  });
+
+  it('contains one component of type GameItem', () => {
+    setTimeout(() => console.log(component.store.getState()), 5000);
+    console.log(component.props);
+    const gameItem = findRenderedDOMComponentWithClass(component, 'game-item');
+
+    expect(gameItem).to.be.ok;
   });
 
 
