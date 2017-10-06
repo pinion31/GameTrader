c4b279c70aa85e8cbf8ed65055616bc8f77b8a38
Chris Cantu <pinion31@gmail.com>
2017-10-05 23:32:44 -0500

    added initial server tests for game routes

---
+++ __mockData__/mockServerData.js
@@ -0,0 +1,57 @@
+module.exports = {
+  newUser01 : {
+      username: 'chris81',
+      password1: 'wonderdog1!',
+      password2: 'wonderdog1!',
+      email: 'me@gmail.com',
+      city: 78412,
+      state: 'Texas',
+    },
+
+  newUser02 : {
+      username: 'lucynoodles',
+      password1: 'wonderdog1!',
+      password2: 'wonderdog1!',
+      email: 'lucyfuzzyface@gmail.com',
+      city: 78653,
+      state: 'Texas',
+    },
+
+  newUser03 : {
+      username: 'nicole81',
+      password1: 'pumpingpussy1!',
+      password2: 'pumpingpussy1!',
+      email: 'mnicole@gmail.com',
+      city: 78412,
+      state: 'Texas',
+    },
+ Fallout4: {
+    'screenshots': [
+        'https://images.igdb.com/igdb/image/upload/t_screenshot_med/whhptvhci1bdoqolofjo.jpg',
+        'https://images.igdb.com/igdb/image/upload/t_screenshot_med/acrxfc2grr69wfbql8ax.jpg',
+        'https://images.igdb.com/igdb/image/upload/t_screenshot_med/ufocunt4ze1rjomybg2h.jpg',
+        'https://images.igdb.com/igdb/image/upload/t_screenshot_med/twgwp1xobnae4kbky2hw.jpg',
+        'https://images.igdb.com/igdb/image/upload/t_screenshot_med/xqhildldpukjvj9gcfmt.jpg'
+    ],
+    'gameConsole' : '49',
+    'cover': 'https://images.igdb.com/igdb/image/upload/t_cover_small/g82nr1m9xqr8wnp0xdrn.jpg',
+    'summary' : 'Bethesda Game Studios, the award-winning creators of Fallout 3 and The Elder Scrolls V: Skyrim, welcome you to the world of Fallout 4 – their most ambitious game ever, and the next generation of open-world gaming.',
+    'id' : 9630,
+    'name' : 'Fallout 4'
+    },
+  RedDead2: {
+      'screenshots': [
+          'https://images.igdb.com/igdb/image/upload/t_screenshot_med/c9xalka7stjkx4mes7kp.jpg',
+          'https://images.igdb.com/igdb/image/upload/t_screenshot_med/whddwtc2bwjpgbokeywn.jpg',
+          'https://images.igdb.com/igdb/image/upload/t_screenshot_med/mmogczn9mfakkb0qjijk.jpg',
+          'https://images.igdb.com/igdb/image/upload/t_screenshot_med/xegpfnsvlyeld0zkjnrc.jpg',
+          'https://images.igdb.com/igdb/image/upload/t_screenshot_med/gn7mnuls0gtq6doutjny.jpg'
+      ],
+      'gameConsole' : '49',
+      'cover': 'https://images.igdb.com/igdb/image/upload/t_cover_small/tvqctso5qxkvfwmq0n1m.jpg',
+      'summary' : 'Developed by the creators of Grand Theft Auto V and Red Dead Redemption, Red Dead Redemption 2 is an epic tale of life in Americas unforgiving heartland.',
+      'id' : 25076,
+      'name' : 'Red Dead Redemption 2'
+  }
+
+};
--- __server_tests__/server.test.js
+++ __server_tests__/server.test.js
@@ -1,13 +1,33 @@
 
 
 const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/local');
+mongoose.Promise = global.Promise;
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../dist/server');
 const should = chai.should();
+const User = require('../dist/models/user');
+const session = require('supertest-session');
+
+const { newUser01, newUser02, newUser03, RedDead2, Fallout4} = require('../__mockData__/mockServerData');
 
 chai.use(chaiHttp);
+const request = require('supertest');
+//let testSession = session(chaiHttp);
+//chai.use(testSession);
+const agent = request.agent('http://localhost:3000');
+beforeEach(function () {
+  //testSession = session(chai);
+});
+
+/*
+beforeEach((done) => {
+  mongoose.connection.collections.gametraders.drop(() => {
+    done();
+  });
+});*/
 
 describe('Log Out', () => {
   it('logs out and destroys session', (done) => {
@@ -18,7 +38,6 @@ describe('Log Out', () => {
         res.text.should.be.eql('session ended');
         done();
       });
-
   });
 });
 
@@ -36,3 +55,119 @@ describe('Find Games', () => {
       });
   });
 });
+
+describe('Add Users To Database', () => {
+  beforeEach((done) => {
+    mongoose.connection.collections.gametraders.drop(() => {
+      done();
+    });
+  });
+
+  it('add a user to database', (done) => {
+    chai.request('http://localhost:3000')
+      .post('/users/addUser')
+      .send(newUser01)
+      .end((err, res) => {
+        res.body.validation.should.be.eql('valid');
+        res.body.redirect.should.be.eql('/Dashboard');
+        done();
+      });
+  });
+
+  it('add two users to database', (done) => {
+    chai.request('http://localhost:3000')
+      .post('/users/addUser')
+      .send(newUser02)
+      .end((err, res) => {
+        chai.request('http://localhost:3000')
+          .post('/users/addUser')
+          .send(newUser03)
+          .end((err, res) => {
+            done();
+          });
+      });
+  });
+
+});
+
+describe('Login', () => {
+  it('logs in user, chris81', (done) => {
+    chai.request('http://localhost:3000')
+      .post('/users/loginUser')
+      .send({username: 'chris81', password: 'wonderdog1!'})
+      .end((err, res) => {
+        res.body.validation.should.be.eql('valid');
+        res.body.redirect.should.be.eql('/Dashboard');
+        done();
+      });
+  });
+});
+
+describe('Add Game', () => {
+  it('adds game to a user', (done) => {
+    agent.post('/users/loginUser')
+      .send({username: 'chris81', password: 'wonderdog1!'})
+      .end((err, res) => {
+        agent.post('/games/addGame')
+          .send([RedDead2])
+          .end((error, response) => {
+            response.should.have.status(200);
+            response.body[0].owner.should.be.eql(newUser01.username);
+            response.body[0].name.should.be.eql(RedDead2.name);
+            response.body[0].id.should.be.eql(RedDead2.id);
+            done();
+          });
+      });
+  });
+});
+
+describe('Add 2nd Game', () => {
+  it('adds game to a user', (done) => {
+    agent.post('/users/loginUser')
+      .send({username: 'chris81', password: 'wonderdog1!'})
+      .end((err, res) => {
+        agent.post('/games/addGame')
+          .send([Fallout4])
+          .end((error, response) => {
+            response.should.have.status(200);
+            response.body[0].owner.should.be.eql(newUser01.username);
+            response.body[0].name.should.be.eql(Fallout4.name);
+            response.body[0].id.should.be.eql(Fallout4.id);
+            done();
+          });
+      });
+  });
+});
+
+describe('retrieve user games', () => {
+  it('gets user games', (done) => {
+    agent.get('/games/getUserGames')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body[0].owner.should.be.eql(newUser01.username);
+        res.body[0].name.should.be.eql(RedDead2.name);
+        res.body[0].id.should.be.eql(RedDead2.id);
+        res.body[1].owner.should.be.eql(newUser01.username);
+        res.body[1].name.should.be.eql(Fallout4.name);
+        res.body[1].id.should.be.eql(Fallout4.id);
+        done();
+      });
+  });
+});
+
+describe('remove game', () => {
+  it('removes a game', (done) => {
+    agent.post('/games/removeGame')
+      .send({id:Fallout4.id})
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.length.should.be.eql(1);
+        res.body[0].owner.should.be.eql(newUser01.username);
+        res.body[0].name.should.be.eql(RedDead2.name);
+        res.body[0].id.should.be.eql(RedDead2.id);
+        done();
+      });
+  });
+});
+
+
